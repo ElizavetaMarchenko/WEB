4b2cb98b0d21ec1cccca829b2538e82d0ba810e1
ElizavetaMarchenko <117085297+ElizavetaMarchenko@users.noreply.github.com>
2023-10-18 21:55:01 +0300

    Add django

---
+++ online_store/store_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def first_page(request):
+   #return HttpResponse("<h3>Hello!</h3>")
+   return render(request, 'first_page.html')
+
+
+def about_us(request):
+   return HttpResponse("<h3>about</h3>")
