ce58ddc5facb5ddf3af04006af231405af7ba718
UUyy-Geniy <jekapeheka@gmail.com>
2023-12-16 02:09:10 +0300

    Add a design template for the productpage

--- online_store/frontend/src/pages/productpage/productpage.js
+++ online_store/frontend/src/pages/productpage/productpage.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { Card, Typography, Row, Col } from 'antd';
 import axios from 'axios';
+import gsap from 'gsap';
+
+const { Title, Paragraph } = Typography;
 
 const ProductPage = () => {
   const { productId } = useParams();
@@ -9,8 +13,9 @@ const ProductPage = () => {
   useEffect(() => {
     const getProductDetails = async () => {
       try {
-        const response = await axios.get(`get_product_details/${productId}`);
+        const response = await axios.get(`/get_product_details/${productId}`);
         setProduct(response.data);
+        animateProductDetails();
       } catch (error) {
         alert('Error getting product details');
       }
@@ -19,17 +24,29 @@ const ProductPage = () => {
     getProductDetails();
   }, [productId]);
 
+  const animateProductDetails = () => {
+    gsap.from('.product-card', { opacity: 0, duration: 1.5, ease: 'power2.inOut' });
+  };
+
   if (!product) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>{product.product_name}</h1>
-      <p>{product.product_description}</p>
-      <p>Price: {product.product_price}р</p>
-      {/* Дополнительная информация о продукте */}
-    </div>
+    <Row gutter={[16, 16]} justify="center">
+      <Col xs={24} sm={16} md={12} lg={8} xl={6}>
+        <Card
+          cover={<img alt={product.product_name} src={product.product_image} />}
+          style={{ borderRadius: 8 }}
+          className="product-card"
+        >
+          <Title level={3}>{product.product_name}</Title>
+          <Paragraph>{product.product_description}</Paragraph>
+          <Paragraph strong>Price: {product.product_price}р</Paragraph>
+          <Paragraph>Category: {product.category.name}</Paragraph>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
